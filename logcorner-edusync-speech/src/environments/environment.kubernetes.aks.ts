23c12545fdc9bc31a2701c7d9e1d06e276d4fa8e
Gora LEYE <leyegora@yahoo.fr>
2022-03-06 12:30:56 +0100

    enable ingress

---
+++ logcorner-edusync-speech/src/environments/environment.kubernetes.aks.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  commandAPI : 'http://localhost:30666/api',
+  queryAPI : 'http://localhost:30591/api',
+  isAuthenticationEnabled: true,
+  hubNotificationUrl : 'https://kubernetes.agic.com/hub-notification-server/logcornerhub',
+  azureAdB2C : {
+    clientId: '5ea677-0470-423f-8013-e58c37142c80',
+    tenantName :'datasynchrob2c'
+  }
+};

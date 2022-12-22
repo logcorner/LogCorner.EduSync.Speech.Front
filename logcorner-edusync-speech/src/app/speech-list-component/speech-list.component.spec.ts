77bdc0f38b7c93d4e61e6b810389a481d4f20231
azurecorner <leyegora@yahoo.fr>
2022-12-22 14:13:58 +0100

    get speeches

--- logcorner-edusync-speech/src/app/speech-list-component/speech-list.component.spec.ts
+++ logcorner-edusync-speech/src/app/speech-list-component/speech-list.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SpeechListComponent } from './speech-list-component';
+import { SpeechListComponent } from './speech-list.component';
 
-describe('SpeechListComponentComponent', () => {
+describe('SpeechListComponent', () => {
   let component: SpeechListComponent;
   let fixture: ComponentFixture<SpeechListComponent>;
 
